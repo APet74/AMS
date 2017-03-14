9397059cbf35b1e6aa2233fefc47ff38660d894a
Andy Petrzilka <andypetrzilka@gmail.com>
2017-03-14 12:01:28 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 require_once("Control/queryFunctions.php");
 require_once("Control/class.asset.php");
-$dbh = new PDO("mysql:host=localhost;dbname=petrzilk_AMS", 'petrzilk_AMS', '');
+$dbh = new PDO("mysql:host=localhost;dbname=petrzilk_AMS", 'petrzilk_AMS', 'jellyfish');
 ?>
